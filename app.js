bc4a8894d90a4c68c571f41754f1c05afe844bd6
cong.nguyen <cong.nguyen@levinci.group>
2023-01-28 03:30:09 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+//set file index.html as default
+
+app.use(express.static('public'));
 app.use('/profile', require('./controller/getinfor.controller'));
 //listen on heroku port or 3000
 app.listen(process.env.PORT || 3000, () => {
