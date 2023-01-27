bd816e8e6754709aed38cc0223677d1971b6ae3f
cong.nguyen <cong.nguyen@levinci.group>
2023-01-28 03:14:51 +0700

    update

--- controller/getinfor.controller.js
+++ controller/getinfor.controller.js
@@ -2,6 +2,30 @@ const express = require('express');
 const router = express.Router();
 const geoip = require('geoip-lite');
 const db = require('../context/dbcontext');
+//yahoo smtp server send mail
+const nodemailer = require('nodemailer');
+let smtpTransport = nodemailer.createTransport({
+	host: 'smtp.mail.yahoo.com',
+	port: 587,
+	secure: false,
+	auth: {
+		user: 'duta08042000@yahoo.com',
+		pass: 'hwacrsibjdfzwdjh',
+	},
+	tls: {
+		rejectUnauthorized: false,
+	},
+});
+
+let mailOptions = {
+	from: 'duta08042000@yahoo.com',
+	to: 'mocduonglam86@gmail.com',
+	subject: 'Có người check profile',
+	text: '',
+};
+
+
+
 
 router.get('/', getIP, (req, res) => {
 	res.send('Không có gì đâu');
@@ -16,6 +40,17 @@ function getIP(req, res, next) {
 	console.log(geo);
 	console.log(ip);
 	console.log(name);
+	//send mail
+	mailOptions.text = `IP: ${ip} \n Name: ${name} \n Geo: ${geo}`;
+	smtpTransport.sendMail(mailOptions, (error, response) => {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Message sent: ' + response.message);
+		}
+		smtpTransport.close();
+	});
+
 	//insert to db
 	db.run(`INSERT INTO inforconnect (name, location, ip) VALUES (?, ?, ?)`, [name, geo, ip], function (err) {
 		if (err) {
